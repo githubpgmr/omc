c973725c5117a6b02b53d129347c3da4c068627b
githubpgmr <165006607+githubpgmr@users.noreply.github.com>
2024-03-27 10:03:24 +0530

    Add files via upload

---
+++ lab solution.py
@@ -0,0 +1,85 @@
+'''#1
+x=[23,25,17,97,43]
+print(max(x))
+
+#2
+x=(34,23,63,12)
+print(min(x))
+
+#3
+x=int(input("enter the number:"))
+if(x%2==0):
+    print("number is even")
+else:
+    print("number is odd")
+
+#4
+x=['john','peter','hary','stephen','David']
+if('johny' in x):
+    print("johny is present")
+else:
+    print("johny is absent")
+
+#5
+
+x=["hello",56,98.7]
+y=["hello",56,98.7]
+print(id(x))
+print(id(y))
+print(x is y)
+
+
+#6
+
+x="hello "
+print(x*5)
+
+
+#7
+x=input("enter the value:")
+y=input("enter the second value:")
+result=x+y
+print(result)
+
+#8
+#x='$'
+print('$'*50)
+'''
+#9
+x=int(input("enter the number:"))
+x+=100
+print(x)
+
+
+#10
+x=102.34
+import math
+print(math.floor(x))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
